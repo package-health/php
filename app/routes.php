780b1958876646fd0d8e258faad79cecb34a02a7
Flavio Heleno <flaviohbatista@gmail.com>
2022-05-04 12:48:57 -0300

    Add catch-all route for 404 errors

--- app/routes.php
+++ app/routes.php
@@ -11,33 +11,58 @@ use App\Application\Action\System\HealthAction;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
+use Slim\Exception\HttpNotFoundException;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return static function (App $app): void {
-  $app->options('/{routes:.*}', function (ServerRequestInterface $request, ResponseInterface $response) {
-    // CORS Pre-Flight OPTIONS Request Handler
-    return $response;
-  });
+  $app->options(
+    '/{routes:.*}',
+    function (ServerRequestInterface $request, ResponseInterface $response): ResponseInterface {
+      // CORS Pre-Flight OPTIONS Request Handler
+      return $response;
+    }
+  );
 
   $app->get('/', ListPackagesAction::class)
     ->setName('listPackages');
 
   $app->get('/health', HealthAction::class);
 
-  $app->group('/packages', function (Group $group) {
-    $group
-      ->get('', RedirectListPackagesAction::class);
-    $group
-      ->get('/{vendor}/{project}/status.svg', RedirectPackageBadgeAction::class)
-      ->setName('redirectPackageBadge');
-    $group
-      ->get('/{vendor}/{project}/{version}/status.svg', ViewPackageBadgeAction::class)
-      ->setName('viewPackageBadge');
-    $group
-      ->get('/{vendor}/{project}', RedirectPackageAction::class)
-      ->setName('redirectPackage');
-    $group
-      ->get('/{vendor}/{project}/{version}', ViewPackageAction::class)
-      ->setName('viewPackage');
-  });
+  $app->group(
+    '/packages',
+    function (Group $group) {
+      $group
+        ->get('', RedirectListPackagesAction::class);
+      $group
+        ->get('/{vendor}/{project}/status.svg', RedirectPackageBadgeAction::class)
+        ->setName('redirectPackageBadge');
+      $group
+        ->get('/{vendor}/{project}/{version}/status.svg', ViewPackageBadgeAction::class)
+        ->setName('viewPackageBadge');
+      $group
+        ->get('/{vendor}/{project}', RedirectPackageAction::class)
+        ->setName('redirectPackage');
+      $group
+        ->get('/{vendor}/{project}/{version}', ViewPackageAction::class)
+        ->setName('viewPackage');
+    }
+  );
+
+  /**
+   * Catch-all route to serve a 404 Not Found page if none of the routes match
+   * NOTE: make sure this route is defined last
+   */
+  $app->map(
+    [
+      'GET',
+      'POST',
+      'PUT',
+      'DELETE',
+      'PATCH'
+    ],
+    '/{routes:.+}',
+    function (ServerRequestInterface $request, ResponseInterface $response): ResponseInterface {
+      throw new HttpNotFoundException($request);
+    }
+  );
 };
