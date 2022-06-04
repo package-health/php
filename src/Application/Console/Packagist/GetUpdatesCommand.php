2e1f205fbec7715f62fe5feb17a65afc4dc5006b
Flavio Heleno <flaviohbatista@gmail.com>
2022-06-04 13:17:45 -0300

    Fixed packagist:get-updates command

--- src/Application/Console/Packagist/GetUpdatesCommand.php
+++ src/Application/Console/Packagist/GetUpdatesCommand.php
@@ -13,6 +13,7 @@ use Exception;
 use InvalidArgumentException;
 use RuntimeException;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -81,17 +82,36 @@ final class GetUpdatesCommand extends Command {
 
       $preference = $preferenceCol[0] ?? null;
       if ($preference === null) {
-        $since = $this->packagist->getChangesTimestamp();
+        $io->text(
+          sprintf(
+            '[%s] Last update not set',
+            date('H:i:s')
+          )
+        );
+
+        // https://packagist.org/apidoc#track-package-updates
+        $updates = $this->packagist->getPackageUpdates(time() * 10000);
         $preference = $this->preferenceRepository->create(
           'packagist',
           'timestamp',
-          (string)$since,
+          (string)$updates['timestamp'],
           PreferenceTypeEnum::isInteger
         );
       }
 
-      $changes = $this->packagist->getPackageUpdates($preference->getValueAsInteger());
-      foreach ($changes['changes'] as $action) {
+      $io->text(
+        sprintf(
+          '[%s] Checkpoint: "<options=bold;fg=cyan>%d</>" (<options=bold;fg=cyan>%s</>)',
+          date('H:i:s'),
+          $preference->getValueAsInteger(),
+          date('H:i:s d/m/Y', (int)($preference->getValueAsInteger() / 10000))
+        )
+      );
+
+      $updates = $this->packagist->getPackageUpdates($preference->getValueAsInteger());
+
+      $packageList = [];
+      foreach ($updates['actions'] as $action) {
         switch ($action['type']) {
           case 'update':
             $packageName = $action['package'];
@@ -99,19 +119,34 @@ final class GetUpdatesCommand extends Command {
               $packageName = substr($packageName, 0, strlen($packageName) - 4);
             }
 
-            $packageCol = $this->packageRepository->find(['name' => $packageName]);
-
-            $package = $packageCol[0] ?? null;
-            if ($package === null) {
-              $package = $this->packageRepository->create($packageName);
+            if (in_array($packageName, $packageList) === true) {
+              continue 2;
             }
 
+            $packageList[] = $packageName;
+
+            $io->text(
+              sprintf(
+                '[%s] Update package: "<options=bold;fg=cyan>%s</>" (%s)',
+                date('H:i:s'),
+                $packageName,
+                date('H:i:s d/m/Y', $action['time'])
+              )
+            );
+
             $this->producer->sendCommand(
-              new PackageDiscoveryCommand($package)
+              new PackageDiscoveryCommand($packageName)
             );
 
             break;
           case 'delete':
+            // $io->text(
+            //   sprintf(
+            //     '[%s] Remove package: "<options=bold;fg=cyan>%s</>"',
+            //     date('H:i:s'),
+            //     $action['package']
+            //   )
+            // );
             // TODO
             // $this->producer->sendCommand(
             //   new PackageRemoveCommand($package);
@@ -119,13 +154,37 @@ final class GetUpdatesCommand extends Command {
 
             break;
           case 'resync':
+            $io->text(
+              sprintf(
+                '[%s] Resync local database',
+                date('H:i:s')
+              )
+            );
+
+            $command = $this->getApplication()->find('packagist:get-list');
+            $returnCode = $command->run(
+              new ArrayInput(
+                [
+                  '--resync' => true,
+                  '--mirror' => $mirror
+                ]
+              ),
+              $output
+            );
+
             break;
           default:
-            //
+            $io->text(
+              sprintf(
+                '[%s] Unknown action type: "<options=bold;fg=cyan>%s</>"',
+                date('H:i:s'),
+                $action['type']
+              )
+            );
         }
       }
 
-      $preference = $preference->withIntegerValue($changes['timestamp']);
+      $preference = $preference->withIntegerValue($updates['timestamp']);
       if ($preference->isDirty()) {
         $this->preferenceRepository->update($preference);
       }
