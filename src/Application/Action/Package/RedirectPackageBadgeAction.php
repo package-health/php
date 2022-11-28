bddb9a889a33dc5751de2bc9254b3ba85cb0ecfe
Flavio Heleno <flaviohbatista@gmail.com>
2022-11-28 10:25:02 -0300

    Fix badge display for packages without tagged releases

--- src/Application/Action/Package/RedirectPackageBadgeAction.php
+++ src/Application/Action/Package/RedirectPackageBadgeAction.php
@@ -4,11 +4,31 @@ declare(strict_types = 1);
 namespace PackageHealth\PHP\Application\Action\Package;
 
 use PackageHealth\PHP\Domain\Package\PackageNotFoundException;
+use PackageHealth\PHP\Domain\Package\PackageRepositoryInterface;
 use PackageHealth\PHP\Domain\Package\PackageValidator;
+use PackageHealth\PHP\Domain\Version\Version;
+use PackageHealth\PHP\Domain\Version\VersionNotFoundException;
+use PackageHealth\PHP\Domain\Version\VersionRepositoryInterface;
+use Psr\Cache\CacheItemPoolInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
+use Slim\HttpCache\CacheProvider;
 use Slim\Routing\RouteContext;
 
 final class RedirectPackageBadgeAction extends AbstractPackageAction {
+  private VersionRepositoryInterface $versionRepository;
+
+  public function __construct(
+    LoggerInterface $logger,
+    CacheProvider $cacheProvider,
+    CacheItemPoolInterface $cacheItemPool,
+    PackageRepositoryInterface $packageRepository,
+    VersionRepositoryInterface $versionRepository
+  ) {
+    parent::__construct($logger, $cacheProvider, $cacheItemPool, $packageRepository);
+    $this->versionRepository = $versionRepository;
+  }
+
   protected function action(): ResponseInterface {
     $vendor = $this->resolveStringArg('vendor');
     PackageValidator::assertValidVendor($vendor);
@@ -33,13 +53,40 @@ final class RedirectPackageBadgeAction extends AbstractPackageAction {
       $this->logger->debug("Badge for package '{$vendor}/{$project}' is being redirected.");
 
       $package = $packageCol->first();
+      $latestVersion = $package->getLatestVersion();
+      if ($latestVersion === '') {
+        $versionCol = $this->versionRepository->find(
+          query: [
+            'package_id' => $package->getId()
+          ],
+          orderBy: [
+            'created_at' => 'DESC'
+          ]
+        );
+
+        // remove branches that starts with "dev-dependabot/" or "dev-renovate/"
+        $versionCol = $versionCol->filter(
+          static function (Version $version): bool {
+            return str_starts_with($version->getNumber(), 'dev-dependabot/') === false &&
+              str_starts_with($version->getNumber(), 'dev-renovate/') === false;
+          }
+        );
+
+        if ($versionCol->isEmpty() === true) {
+          throw new VersionNotFoundException();
+        }
+
+        $version = $versionCol->first();
+        $latestVersion = $version->getNumber();
+      }
+
       $routeParser = RouteContext::fromRequest($this->request)->getRouteParser();
       $url = $routeParser->urlFor(
         'viewPackageBadge',
         [
           'vendor'  => $vendor,
           'project' => $project,
-          'version' => (string)($package->getLatestVersion() ?: 'unknown')
+          'version' => $latestVersion
         ]
       );
 
